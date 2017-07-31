cb036aa664110702e6030b0b9482e0b0c230d1df
Rasmus Edgar Fink Jørgensen <refj@stofa.dk>
2017-07-31 23:42:01 +0200

    Set wbar placement, set borderless

--- wbar/wbar.c
+++ wbar/wbar.c
@@ -9,23 +9,29 @@
 
 int main(void)
 {
-	struct TagItem tagitem[3];
+	struct TagItem tagitem[5];
 	unsigned long ilock = LockIBase(0L);
 	struct Screen *screen = LockPubScreen(NULL);
 	struct Window *window;
         int swidth = screen->Width;
+        int sheight = screen->Height;
+        int wbarheight = 20;
 
         // printf("swidth: %d\n", swidth);
         tagitem[0].ti_Tag = WA_Width;
         tagitem[0].ti_Data = swidth;
         tagitem[1].ti_Tag = WA_Height;
-        tagitem[1].ti_Data = 20;
-        tagitem[2].ti_Tag = TAG_DONE;
+        tagitem[1].ti_Data = wbarheight;
+        tagitem[2].ti_Tag = WA_Top;
+        tagitem[2].ti_Data = sheight - wbarheight;
+        tagitem[3].ti_Tag = WA_Borderless;
+        tagitem[3].ti_Data = 1;
+        tagitem[4].ti_Tag = TAG_DONE;
 
         if (window = OpenWindowTagList(NULL, tagitem)) { 
-		Delay(500);
+		Delay(200);
+        	CloseWindow(window);
 	}
-        CloseWindow(window);
 
 	// Unlock Pubscreen and Intuitionbase both return nothing
 	UnlockPubScreen(NULL, screen);
